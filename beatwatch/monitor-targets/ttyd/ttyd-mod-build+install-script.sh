45b85be14cd58be6329731f4c75e67903494cd3e
HAYAMI <noname@example.com>
2022-12-27 19:29:03 +0900

    save

--- beatwatch/monitor-targets/ttyd/ttyd-mod-build+install-script.sh
+++ beatwatch/monitor-targets/ttyd/ttyd-mod-build+install-script.sh
@@ -3,8 +3,24 @@
 tooldir=${1:-"$HOME/www-tools"}
 patchdir=${2:-"$(pwd)"}
 
+### see following URLs for latest releases
+###   libuv		https://github.com/libuv/libuv/releases
+###   json-c		https://s3.amazonaws.com/json-c_releases/releases/index.html
+###   libwebsockets	https://libwebsockets.org/git/libwebsockets/summary
+###   ttyd		https://github.com/tsl0922/ttyd/releases
+libuv_version='v1.44.2'
+jsonc_version='0.16'
+libwebsockets_version='v4.3-stable'
+ttyd_version='1.7.2'
+
+libuv_download_url=https://dist.libuv.org/dist/${libuv_version}/libuv-${libuv_version}.tar.gz
+jsonc_download_url=https://s3.amazonaws.com/json-c_releases/releases/json-c-${jsonc_version}.tar.gz
+libwebsockets_git_url=https://libwebsockets.org/repo/libwebsockets
+ttyd_download_url=https://github.com/tsl0922/ttyd/archive/refs/tags/${ttyd_version}.tar.gz
+
+
 umask 022
-PATH=/usr/bin:/bin
+PATH=/usr/local/bin:/usr/bin:/bin
 for x in $(env | sed 's/=.*$//'); do
     case "$x" in
     (HOME|HOSTTYPE|LOGNAME|OSTYPE|PATH|PWD|TERM|TMPDIR|TZ|USER|_) ;;
@@ -13,35 +29,169 @@ for x in $(env | sed 's/=.*$//'); do
 done
 
 if [ -z "$TMPDIR"]; then
-    mkdir -p $HOME/tmp
     TMPDIR=$HOME/tmp
     export TMPDIR
 fi
-
-# ttyd version
-v=1.7.2
+mkdir -p $TMPDIR
 
 set -e
 set -x
+
+
+###
+### use gmake if available
+###
+make=make
+if which gmake; then
+    make=gmake
+fi
+
+
+###
+### create a temporary directory for dependent libraries
+###
+depdir=$TMPDIR/ttyd-${ttyd_version}-dependencies
+rm -rf $depdir
+mkdir -p $depdir
+
+
+###
+### check for -ldl
+###
+use_libdl=0
+cd $depdir
+cat > libdl-test.c << 'EOF'
+#include <dlfcn.h>
+int main() { void *handle = 0; dlclose(handle); return 0; }
+EOF
+if cc libdl-test.c -o libdl-test.o > /dev/null 2>&1; then
+    :
+else
+    if cc libdl-test.c -o libdl-test.o -ldl > /dev/null 2>&1; then
+        use_libdl=1
+    else
+        echo '*** ERROR: can not link with/without -ldl' 1>&2
+        exit 1
+    fi
+fi
+echo use_libdl=$use_libdl
+
+
+###
+### build libuv
+###
+cd $depdir
+curl -L -o libuv-${libuv_version}.tar.gz ${libuv_download_url}
+tar -xpzf libuv-${libuv_version}.tar.gz
+mkdir libuv-${libuv_version}-build libuv-${libuv_version}-root
+cd libuv-${libuv_version}-build
+cmake \
+    -DCMAKE_INSTALL_PREFIX=$depdir/libuv-${libuv_version}-root	\
+    -DBUILD_TESTING=OFF						\
+    -DCMAKE_BUILD_TYPE=Release					\
+    ../libuv-${libuv_version}
+cmake --build .
+$make install
+cd $depdir/libuv-${libuv_version}-root
+rm lib/libuv.so.*
+rm lib/libuv.so
+rm lib/pkgconfig/libuv.pc
+
+
+###
+### build libjson-c
+###
+mkdir -p $depdir
+cd $depdir
+curl -L -o json-c-${jsonc_version}.tar.gz ${jsonc_download_url}
+tar -xpzf json-c-${jsonc_version}.tar.gz
+mkdir json-c-${jsonc_version}-build json-c-${jsonc_version}-root
+cd json-c-${jsonc_version}-build
+cmake \
+    -DCMAKE_INSTALL_PREFIX=$depdir/json-c-${jsonc_version}-root	\
+    -DBUILD_SHARED_LIBS=OFF					\
+    -DCMAKE_BUILD_TYPE=Release					\
+    ../json-c-${jsonc_version}
+$make all
+$make USE_VALGRIND=0 test
+$make install
+
+
+###
+### build libwebsockets
+###
+mkdir -p $depdir
+cd $depdir
+rm -rf $HOME/.cmake/packages/libwebsockets
+rmdir $HOME/.cmake/packages 2> /dev/null || :
+rmdir $HOME/.cmake          2> /dev/null || :
+git clone --depth 1			\
+    --branch ${libwebsockets_version}	\
+    ${libwebsockets_git_url}		\
+    libwebsockets-${libwebsockets_version}
+[ $use_libdl -eq 0 ] || patch -d libwebsockets-${libwebsockets_version} \
+                              -p1 < $patchdir/ttyd-mod-libwebsockets-dl.patch
+lwsroot=$depdir/libwebsockets-${libwebsockets_version}-root
+luvroot=$depdir/libuv-${libuv_version}-root
+mkdir libwebsockets-${libwebsockets_version}-build $lwsroot
+cd libwebsockets-${libwebsockets_version}-build
+cmake \
+    -DCMAKE_INSTALL_PREFIX:PATH=$lwsroot		\
+    -DLWS_WITH_SHARED=0					\
+    -DLWS_WITH_STATIC=1					\
+    -DLWS_WITH_SSL=0					\
+    -DLWS_WITH_LIBUV=1					\
+    -DLWS_LIBUV_INCLUDE_DIRS=$luvroot/include		\
+    -DLWS_LIBUV_LIBRARIES=$luvroot/lib/libuv_a.a	\
+    -DCMAKE_BUILD_TYPE=Release				\
+    ../libwebsockets-${libwebsockets_version}
+$make -v
+$make install
+rm -rf $HOME/.cmake/packages/libwebsockets
+rmdir $HOME/.cmake/packages 2> /dev/null || :
+rmdir $HOME/.cmake          2> /dev/null || :
+
+
+###
+### build ttyd (install as ttyd-mod)
+###
 cd $TMPDIR
-rm -f ttyd-$v.tar.gz
-rm -rf ttyd-$v
-curl -L -o ttyd-$v.tar.gz \
-    https://github.com/tsl0922/ttyd/archive/refs/tags/$v.tar.gz
-tar -xpzf ttyd-$v.tar.gz
-cd ttyd-$v
+rm -f ttyd-${ttyd_version}.tar.gz
+rm -rf ttyd-${ttyd_version}
+curl -L -o ttyd-${ttyd_version}.tar.gz ${ttyd_download_url}
+tar -xpzf ttyd-${ttyd_version}.tar.gz
+cd ttyd-${ttyd_version}
 for i in $patchdir/ttyd-mod-[0-9]*.patch; do
-    patch -p0 < $i
+    patch -p1 < $i
 done
-rm -rf build
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=$tooldir ..
-cat $patchdir/ttyd-mod-install.patch	\
-    | sed -e "s|%%%|$TMPDIR/ttyd-$v|g"	\
-    | patch -p0
-make
-make install
+[ $use_libdl -eq 0 ] || patch -p1 < $patchdir/ttyd-mod-cap+dl.patch
+cd $TMPDIR
+rm -rf ttyd-${ttyd_version}-build
+mkdir ttyd-${ttyd_version}-build
+cd ttyd-${ttyd_version}-build
+luvroot=$depdir/libuv-${libuv_version}-root
+ljcroot=$depdir/json-c-${jsonc_version}-root
+lwsroot=$depdir/libwebsockets-${libwebsockets_version}-root
+cmake \
+    -DCMAKE_INSTALL_PREFIX=$tooldir ..				\
+    -DLIBUV_INCLUDE_DIR=$luvroot/include			\
+    -DLIBUV_LIBRARY=$luvroot/lib/libuv_a.a			\
+    -DJSON-C_INCLUDE_DIR=$ljcroot/include/json-c		\
+    -DJSON-C_LIBRARY=$ljcroot/lib/libjson-c.a			\
+    -DLIBWEBSOCKETS_INCLUDE_DIR=$lwsroot/include		\
+    -DLIBWEBSOCKETS_LIBRARY=$lwsroot/lib/libwebsockets.a	\
+    -DLWS_OPENSSL_ENABLED=0					\
+    -DLWS_MBEDTLS_ENABLED=0					\
+    -DCMAKE_BUILD_TYPE=Release					\
+    ../ttyd-${ttyd_version}
+sed -i.sedorig						\
+    -e 's|PREFIX}/bin/ttyd"|PREFIX}/bin/ttyd-mod"|'	\
+    -e 's|build/ttyd"|build/ttyd" RENAME "ttyd-mod"|'	\
+    -e 's|man/ttyd.1"|man/ttyd.1" RENAME "ttyd-mod.1"|'	\
+    -e 's| TYPE EXECUTABLE | TYPE PROGRAM |'		\
+    cmake_install.cmake
+$make
+$make install
 exit 0
 
 
